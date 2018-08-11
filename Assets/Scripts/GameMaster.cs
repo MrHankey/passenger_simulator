46168b5c15f4d6d9e900bcf3027c42f16f919d59
Etienne Ott <eti.ott@gmail.com>
2018-08-11 17:04:35 +0200

    Add Menu scene and GameMaster object.

---
+++ Assets/Scripts/GameMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+	GameObject train;
+
+	// Use this for initialization
+	void Start () {
+		train = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
