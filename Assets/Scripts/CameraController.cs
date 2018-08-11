6450643cb7781aaefccb850ddcaccaf2fca9cfdf
Raphael Leiteritz <leiterrl@ipvs.uni-stuttgart.de>
2018-08-11 16:49:27 +0200

    Add vertical camera movement

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,11 +9,16 @@ public class CameraController : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        offset = transform.position - player.transform.position;
+        //offset = transform.position - player.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-        transform.position = player.transform.position + offset;
-	}
+        //transform.position = player.transform.position + offset;
+        float rotationY = Input.GetAxis("Mouse Y") * 5.0f;
+        rotationY *= -1.0f;
+        //rotation = Mathf.Clamp(rotationY, -90, 90);
+
+        transform.Rotate(new Vector3(rotationY, 0.0f, 0.0f));
+    }
 }
