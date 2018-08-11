c4024c5a98da4a7d3b010b04bafbb7ada2ad8a15
Etienne Ott <eti.ott@gmail.com>
2018-08-11 18:36:03 +0200

    Implement first train control and place some trash.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -8,11 +8,20 @@ public class GameMaster : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		train = GameObject.FindGameObjectWithTag("Player");
+		train = GameObject.FindGameObjectWithTag("Train");
+		train.transform.position += new Vector3(-100f, 0f, 0f);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if (train.transform.position.x < -50f) {
+			train.transform.position += new Vector3(Time.deltaTime * 10.0f, 0f, 0f);
+		} else if (train.transform.position.x >= -50f && train.transform.position.x < -20f) {
+			train.transform.position += new Vector3(Time.deltaTime * 5.0f, 0f, 0f);
+		} else if (train.transform.position.x >= -20f && train.transform.position.x < -5f) {
+			train.transform.position += new Vector3(Time.deltaTime * 2.5f, 0f, 0f);
+		} else if (train.transform.position.x >= -5f && train.transform.position.x < 0f) {
+			train.transform.position += new Vector3(Time.deltaTime * 1f, 0f, 0f);
+		}
 	}
 }
