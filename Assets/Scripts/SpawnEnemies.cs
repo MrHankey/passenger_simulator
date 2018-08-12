b3dc6adf619b7b9a86bab91ecec57bb9c2425ad8
qres <qres@users.noreply.github.com>
2018-08-12 18:44:58 +0200

    add spawn points to test scene

---
+++ Assets/Scripts/SpawnEnemies.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour {
+
+    public GameObject enemy;
+    public Transform[] spawn_points;
+    public float start_time;
+    public float interval;
+
+    // Use this for initialization
+    void Start () {
+        InvokeRepeating("Spawn", start_time, interval);
+    }
+
+    // Update is called once per frame
+    void Spawn () {
+        int index = Random.Range(0, spawn_points.Length);
+
+        Vector3    pos = spawn_points[index].position;
+        Quaternion rot = spawn_points[index].rotation;
+        pos.x += (Random.value * 2.0f - 1.0f) * GetComponent<Transform>().localScale.x;
+        pos.z += (Random.value * 2.0f - 1.0f) * GetComponent<Transform>().localScale.z;
+        GameObject o = Instantiate(enemy, pos, rot);
+        Transform trans = o.GetComponent<Transform>();
+    }
+}
