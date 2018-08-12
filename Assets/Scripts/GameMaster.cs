d5c605fb1a762766b1b7d7100698f80ea8ec51e6
Etienne Ott <eti.ott@gmail.com>
2018-08-12 18:39:39 +0200

    Fix train movement not transporting the player and other rigid bodies.

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -25,7 +25,6 @@ public class GameMaster : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		startTime = -1f;
 		status = TrainStatus.Incoming;
 
 		GameObject canvas = GameObject.FindGameObjectWithTag("UICanvas");
@@ -40,45 +39,59 @@ public class GameMaster : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		float vel = System.Math.Max(minVelocity, System.Math.Abs(train.transform.position.x));
+	void FixedUpdate () {
+		float vel;
 
-		if (startTime < 0f) {
-			train.transform.position += new Vector3(
-				Time.deltaTime * vel * velocityFactor,
-				0f,
-				0f
-			);
-
-			if (train.transform.position.x >= 0f) {
-				startTime = Time.time;
-			}
-		}
-
-		if (Time.time - startTime > departureTime) {
-			train.transform.position += new Vector3(
-				Time.deltaTime * 2 * vel * velocityFactor,
-				0f,
-				0f
-			);
-		}
-
-		if (Time.time - startTime > departureTime + 10f) {
-			GameObject canvas = GameObject.FindGameObjectWithTag("UICanvas");
-			canvas.GetComponent<Canvas>().enabled = true;
-
-			GameObject text = GameObject.Find("Canvas/Result");
-			text.GetComponent<Text>().text = "Loss";
-
-			GameObject player = GameObject.FindGameObjectWithTag("Player");
-			foreach (GameObject zone in playerGoals) {
-				if (player.GetComponent<Collider>().bounds.Intersects(
-					zone.GetComponent<Collider>().bounds
-				)) {
-					text.GetComponent<Text>().text = "Win";
-					break;
+		switch (status)
+		{
+			case TrainStatus.Incoming:
+				vel = System.Math.Max(minVelocity, System.Math.Abs(train.transform.position.x));
+				train.transform.position += new Vector3(Time.deltaTime * vel * velocityFactor, 0f, 0f);
+				if (train.transform.position.x >= 0f) {
+					startTime = Time.time;
+					status = TrainStatus.DoorsOpening;
 				}
-			}
+				break;
+
+			case TrainStatus.DoorsOpening:
+				status = TrainStatus.Waiting;
+				break;
+
+			case TrainStatus.Waiting:
+				if (Time.time - startTime > departureTime) {
+					status = TrainStatus.DoorsClosing;
+				}
+				break;
+
+			case TrainStatus.DoorsClosing:
+				status = TrainStatus.Outgoing;
+				break;
+
+			case TrainStatus.Outgoing:
+				vel = System.Math.Max(minVelocity, 2f * System.Math.Abs(train.transform.position.x));
+				train.transform.position += new Vector3(Time.deltaTime * vel * velocityFactor, 0f, 0f);
+
+				if (Time.time - startTime > departureTime + 10f) {
+					GameObject canvas = GameObject.FindGameObjectWithTag("UICanvas");
+					canvas.GetComponent<Canvas>().enabled = true;
+
+					GameObject text = GameObject.Find("Canvas/Result");
+					text.GetComponent<Text>().text = "Loss";
+
+					GameObject player = GameObject.FindGameObjectWithTag("Player");
+					foreach (GameObject zone in playerGoals) {
+						if (player.GetComponent<Collider>().bounds.Intersects(
+							zone.GetComponent<Collider>().bounds
+						)) {
+							text.GetComponent<Text>().text = "Win";
+							break;
+						}
+					}
+				}
+				break;
+
+			default:
+				break;
 		}
 	}
 }
