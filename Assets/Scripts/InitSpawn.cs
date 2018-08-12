45cd0f465d2d275179cb2410410444044713e147
qres <qres@users.noreply.github.com>
2018-08-12 20:44:49 +0200

    simple spawn on initialization
    just spawn randomly on plane

---
+++ Assets/Scripts/InitSpawn.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitSpawn : MonoBehaviour {
+
+    public GameObject enemy;
+    public int count;
+    private const float scale_factor = 5f; // standart size of plane is 10x10
+
+	void Start () {
+        Transform trans = GetComponent<Transform>();
+        Vector3 pos = trans.position;
+        Vector3 scale = trans.localScale * scale_factor;
+        Quaternion rot = trans.rotation;
+        for (int i = 0; i < count; i++) {
+            float x = pos.x + (Random.value * 2 - 1) * scale.x;
+            float y = pos.y + 1.0f;
+            float z = pos.z + (Random.value * 2 - 1) * scale.z;
+            GameObject o = Instantiate(enemy, new Vector3(x,y,z), rot);
+        }
+	}
+}
