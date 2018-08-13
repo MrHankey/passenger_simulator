404102ba839c307c8e8b09f87767a741190703d4
Raphael Leiteritz <leiterrl@ipvs.uni-stuttgart.de>
2018-08-13 15:31:25 +0200

    fix spawn logic

--- Assets/Scripts/InitSpawn.cs
+++ Assets/Scripts/InitSpawn.cs
@@ -5,9 +5,10 @@ using UnityEngine;
 public class InitSpawn : MonoBehaviour {
 
     public GameObject enemy;
+    public GameObject aggroEnemy;
     public int count_x = 2;
     public int count_z = 2;
-    private const float scale_factor = 0.5f; // standart size of plane is 10x10
+    private const float scale_factor = 1.0f; // standart size of plane is 10x10
 
 	void Start () {
         Transform trans = GetComponent<Transform>();
@@ -22,10 +23,19 @@ public class InitSpawn : MonoBehaviour {
         {
             for (int j = 0; j < count_z; j++)
             {
-                float x = pos.x + i * (size.x / count_x);// + (Random.value * 2 - 1);
-                float y = pos.y + 1.0f;
-                float z = pos.z + j * (size.z / count_z);// + (Random.value * 2 - 1);
-                Instantiate(enemy, new Vector3(x, y, z), rot);
+                GameObject newEnemy;
+                float x = pos.x + i * (size.x / count_x) - size.x / 2.0f + (Random.value * 2.0f - 1.0f)*0.5f;
+                float y = pos.y + 1.5f;
+                float z = pos.z + j * (size.z / count_z) - size.z/2.0f + (Random.value * 2.0f - 1.0f)*0.5f;
+                if (Random.value > 0.5)
+                {
+                    newEnemy = Instantiate(enemy, new Vector3(x, y, z), rot);
+                }
+                else
+                {
+                    newEnemy = Instantiate(aggroEnemy, new Vector3(x, y, z), rot);
+                }
+                newEnemy.transform.localScale += new Vector3(0.0f, Random.value * 0.3f - 0.2f, 0.0f);
             }
         }
 	}
