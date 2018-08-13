fadcae0bca0d873758594298ae7104a963b8c197
Raphael Leiteritz <leiterrl@ipvs.uni-stuttgart.de>
2018-08-13 10:25:26 +0200

    Fix scaling issue when holding onto train platform

--- Assets/Scripts/HoldOntoColliders.cs
+++ Assets/Scripts/HoldOntoColliders.cs
@@ -5,7 +5,12 @@ using UnityEngine;
 public class HoldOntoColliders : MonoBehaviour {
 
 	void OnTriggerEnter(Collider col) {
-		col.transform.parent.position = gameObject.transform.position;
+        //Vector3 scaleInv = new Vector3(1.0f / gameObject.transform.localScale.x, 1.0f / gameObject.transform.localScale.y, 1.0f / gameObject.transform.localScale.z);
+        //col.transform.localScale = Vector3.Scale(col.transform.localScale, scaleInv);
+        GameObject newObj = new GameObject();
+        newObj.transform.parent = gameObject.transform;
+        col.transform.parent = newObj.transform;
+        
 	}
 
 	void OnTriggerExit(Collider col) {
